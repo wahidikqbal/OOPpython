7f6494136ce3154f85ae97a5a5d227eb2ed4a7e4
wahidikqbal <wahidikqbal@gmail.com>
2019-11-13 13:05:30 +0700

    create 4.method.py
    membuat metode di python

---
+++ 4.method.py
@@ -0,0 +1,30 @@
+class Hero:
+    jumlahHero = 0
+
+    def __init__(self, inputName, inputPower, inputArmor, inputHealth):
+        self.name       =   inputName
+        self.power      =   inputPower
+        self.armor      =   inputArmor
+        self.health     =   inputHealth
+        Hero.jumlahHero +=  1
+
+    def whoIs(self):
+        print("saya adalah " + self.name)
+
+    def healthUp(self, up):
+        self.health += up
+
+    def healthScore(self):
+        return self.health
+
+spiderman   =   Hero("Spiderman", 75, 50, 80)
+superman    =   Hero("Superman", 80, 75, 85)
+hulk        =   Hero("  Hulk", 75, 85, 80)
+
+print(spiderman.name)
+
+superman.whoIs()
+
+hulk.healthUp(10)
+
+print(hulk.healthScore())
