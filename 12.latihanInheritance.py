df8ad3e9370c848167ec07bb280a2ec4efd5f375
wahidikqbal <wahidikqbal@gmail.com>
2019-11-21 22:28:46 +0700

    Update 12.latihanInheritance.py

--- 12.latihanInheritance.py
+++ 12.latihanInheritance.py
@@ -3,9 +3,9 @@ class Hero:
     __jumlahHero = 0
 
     def __init__ (self, name):
-        self.health_pool     =       [0,100,200,300,400,500]
-        self.armor_pool      =       [0,10,20,40,60,80]
-        self.power_pool      =       [0,15,30,45,60,75]
+        self.healthPool     =       [0,100,200,300,400,500]
+        self.armorPool      =       [0,10,20,40,60,80]
+        self.powerPool      =       [0,15,30,45,60,75]
         self.__name         =       name
         self.__exp          =       0
         self.__level        =       0
@@ -21,15 +21,15 @@ class Hero:
         )
 
     @property
-    def health_pool(self):
+    def healthPool(self):
         pass
 
     @property
-    def armor_pool(self):
+    def armorPool(self):
         pass
 
     @property
-    def power_pool(self):
+    def powerPool(self):
         pass
 
     @property
@@ -40,39 +40,39 @@ class Hero:
     def gainExp(self):
         pass
     
-    @health_pool.setter
-    def health_pool(self, input):
-        self.__health_pool = input
+    @healthPool.setter
+    def healthPool(self, input):
+        self.__healthPool = input
 
-    @armor_pool.setter
-    def armor_pool(self, input):
-        self.__armor_pool = input
+    @armorPool.setter
+    def armorPool(self, input):
+        self.__armorPool = input
     
-    @power_pool.setter
-    def power_pool(self, input):
-        self.__power_pool = input
+    @powerPool.setter
+    def powerPool(self, input):
+        self.__powerPool = input
     
     @gainExp.setter
     def gainExp(self, input):
         self.__exp += input
         if self.__exp >= 100:
             self.__levelUp = self.__exp // 100
-            self.__exp %= 100
+            self.__exp     %= 100
     
     @levelUp.setter
     def levelUp (self, input):
         self.__level += input
-        self.__health   = self.__health_pool[self.__level]
-        self.__armor    = self.__armor_pool[self.__level]
-        self.__power    = self.__power_pool[self.__level]
+        self.__health   = self.__healthPool[self.__level]
+        self.__armor    = self.__armorPool[self.__level]
+        self.__power    = self.__powerPool[self.__level]
 
 class HeroIntelligent(Hero):
     
     def __init__(self, name):
         super().__init__(name)
-        self.health_pool = [0, 50, 100, 150, 200, 300]
-        self.armor_pool = [0, 10, 20, 30, 40, 50]
-        self.power_pool = [0, 20, 30, 40, 50, 60]
+        self.healthPool = [0, 50, 100, 150, 200, 300]
+        self.armorPool = [0, 10, 20, 30, 40, 50]
+        self.powerPool = [0, 20, 30, 40, 50, 60]
         self.levelUp =  1
 
 
@@ -80,18 +80,20 @@ class HeroStrength(Hero):
 
     def __init__(self, name):
         super().__init__(name)
-        self.health_pool = [0, 100, 200, 300, 400, 500]
-        self.armor_pool = [0, 20, 30, 40, 50, 60]
-        self.power_pool = [0, 25, 35, 45, 55, 65]
+        self.healthPool = [0, 100, 200, 300, 400, 500]
+        self.armorPool = [0, 20, 30, 40, 50, 60]
+        self.powerPool = [0, 25, 35, 45, 55, 65]
         self.levelUp = 1
 
 
 
 
 hero1 = HeroIntelligent('SPIDERMAN')
-hero1.show_info()
-hero1.gainExp = 200
 
 hero1.show_info()
 print(hero1.__dict__)
 
+hero1.gainExp = 200
+
+hero1.show_info()
+print(hero1.__dict__)
